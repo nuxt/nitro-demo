1338dd725bdf5650616ba8b42453930df6e12015
Pooya Parsa <pyapar@gmail.com>
2020-11-04 20:48:38 +0100

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+export default {
+  serverless: {
+    targets: [
+      'vercel'
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Hello Nuxt!
+    <hr />
+    <span>Generated on {{ t }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      t: new Date()
+    }
+  },
+}
+</script>
