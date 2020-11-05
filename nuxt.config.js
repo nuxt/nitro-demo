62a6b45569dc5f8e393c9edc58552118e401e949
Pooya Parsa <pyapar@gmail.com>
2020-11-05 19:25:17 +0100

    update example

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,5 +6,8 @@ export default {
   ],
 
   serverless: {
+    static: [
+      '/1'
+    ]
   }
 }
---
+++ pages/_i.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello Nuxt</h1>
+    <NLink v-if="i>0" :to="`./${i-1}`">
+      Previous
+    </NLink>
+    Page {{ i }}
+    <NLink v-if="i<10" :to="`./${i+1}`">
+      Next
+    </NLink>
+    <hr />
+    <span>Generated on {{ t }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      t: new Date()
+    }
+  },
+  computed: {
+    i () {
+      return parseInt(this.$route.params.i) || 0
+    }
+  }
+}
+</script>
