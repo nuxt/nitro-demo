8903b5a5911f10ab44ae40f2c846587f91602056
pi0 <pyapar@gmail.com>
2020-11-06 17:44:28 +0000

    Deploying to gh-pages from  @ 9f4ff00a3b1e453a5cfddbb5eaff4be24758cc0a 🚀

--- _nuxt/static/1604684572/2/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/2", {data:[],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1604684656/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{quote:{quote:"Either write something worth reading or do something worth writing.",author:"Benjamin Franklin"}}],fetch:[],mutations:void 0});
