d7b3dcd3ec4e688c2ca60d5d33c9f4594a214d81
pi0 <pyapar@gmail.com>
2020-11-10 22:23:24 +0000

    Deploying to gh-pages from  @ 3398f08f7863c4eb431eb3a91b230defc18f5a06 🚀

---
+++ _nuxt/static/1605046992/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{quote:{quote:"When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.",author:"John Lennon"}}],fetch:[],mutations:void 0});
