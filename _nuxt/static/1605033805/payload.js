5b4076f4874db083c1afc4afb2775420cd91bd32
pi0 <pyapar@gmail.com>
2020-11-10 19:26:08 +0000

    Deploying to gh-pages from  @ 0db72f75972ad95a7b852edd55dee6e7649d3e14 🚀

--- _nuxt/static/1605033805/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/", {data:[{quote:{quote:"The best time to plant a tree was 20 years ago. The second best time is now.",author:"Chinese Proverb"}}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1605036356/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{quote:{quote:"Life is what happens to you while you’re busy making other plans.",author:"John Lennon"}}],fetch:[],mutations:void 0});
