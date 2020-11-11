c4aca0f02be82312ac45f4d1ea3f6135f1dec47c
Pooya Parsa <pyapar@gmail.com>
2020-11-11 12:18:42 +0100

    remove static notice since not working with workers yet

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,6 @@
   <div>
     <p>
       This website is deployed with <a href="https://github.com/nuxt/serverless" target="_blank" role="noopener">nuxt/serverless</a> and nuxt@2
-      (this page is generated at build time)
     </p>
   </div>
 </template>
