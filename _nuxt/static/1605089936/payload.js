85c9ce97f32ecfc214938c241fab99f405bda364
pi0 <pyapar@gmail.com>
2020-11-11 10:22:01 +0000

    Deploying to gh-pages from  @ 5b27a1b6d85c3942aa223f7c0d06fb3073fa0f45 🚀

--- _nuxt/static/1605089936/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/", {data:[{quote:{quote:"Few things can help an individual more than to place responsibility on him, and to let him know that you trust him.",author:"Booker T. Washington"}}],fetch:[],mutations:void 0});
