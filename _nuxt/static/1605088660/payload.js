785915b9ae68a58f752cb01665274685caba9c27
pi0 <pyapar@gmail.com>
2020-11-11 10:03:30 +0000

    Deploying to gh-pages from  @ fb3d0b1e99e0adafb1dc98c8f7688c82e1d07cbc 🚀

--- _nuxt/static/1605088660/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/", {data:[{quote:{quote:"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.",author:"Michael Jordan"}}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1605088998/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{quote:{quote:"Life isn’t about getting and having, it’s about giving and being.",author:"Kevin Kruse"}}],fetch:[],mutations:void 0});
