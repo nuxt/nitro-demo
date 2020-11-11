fd252f4838af6e3774765445d0288c8f997d51a3
Pooya Parsa <pyapar@gmail.com>
2020-11-11 10:34:06 +0100

    refactor: move timeAgo to utils

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,19 +28,7 @@
 </template>
 
 <script>
-import { format } from 'timeago.js'
-
-const timeAgo = (t) => {
-  const diff =  new Date() - this.t
-  const diffSeconds = Math.round(diff/1000)
-  if (diffSeconds > 10) {
-    return format(t)
-  }
-  if (diffSeconds < 1) {
-    return 'just now'
-  }
-  return diffSeconds + ' seconds ago'
-}
+import { timeAgo } from '~/utils'
 
 export default {
   head: {
---
+++ utils/index.js
@@ -0,0 +1,13 @@
+import { format } from 'timeago.js'
+
+export function timeAgo (t){
+  const diff = new Date() - t
+  const diffSeconds = Math.round(diff / 1000)
+  if (diffSeconds > 10) {
+    return format(t)
+  }
+  if (diffSeconds < 1) {
+    return 'just now'
+  }
+  return diffSeconds + ' seconds ago'
+}
