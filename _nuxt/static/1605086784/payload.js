2b029ab02c296c48229695a6d13c76c2f433146e
pi0 <pyapar@gmail.com>
2020-11-11 09:31:13 +0000

    Deploying to gh-pages from  @ dd9c97b79e1e1c6c710e9e35ebf1c8c93da43092 🚀

--- _nuxt/static/1605086784/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/", {data:[{quote:{quote:"Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it.",author:"Johann Wolfgang von Goethe"}}],fetch:[],mutations:void 0});
